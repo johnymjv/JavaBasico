57177f7fa1401032944fe4ed6e266a393644387f
corre_000 <corre_000@portatil-johny>
2018-07-14 09:41:03 -0500

    final punto tres

--- src/com/bancodebogota/fdm/javabasico/Main.java
+++ src/com/bancodebogota/fdm/javabasico/Main.java
@@ -18,63 +18,32 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-      try{  
-        
-        ListaSimple<String> lista=new ListaSimple("1");
-                
-        
-        
-        lista.insertarNodo("2");
-        lista.insertarNodo("3");
-        lista.insertarNodo("4");
-        lista.insertarNodo("5");
-        lista.insertarNodo("6");
-        lista.insertarNodo("7");
-        lista.insertarNodo("8");
-        lista.insertarNodo("9");
-        lista.insertarNodo("10");
-        
-        while(lista.hasNext()) {       
-    System.out.println(lista.next()+" Con Iterator");
+        try {
+
+            ListaSimple<String> lista = new ListaSimple("1");
+
+            for (int i = 2; i <= 10; i++) {
+                lista.insertarNodo(i + "");
+            }
+
+            for (String valor : lista) {
+                System.out.println(valor);
+            }
+
+            // lista.listar();
+            ListaArray<String> listaArray = new ListaArray();
+            for (int i = 0; i < 18; i++) {
+                listaArray.insertar(i + "");
+            }
+
+            for (String valor : listaArray) {
+                System.out.println(valor);
+
+            }
+
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
         }
 
-        
-                 
-       
-       // lista.listar();
-        
-        ListaArray<String> listaArray=new ListaArray();
-        listaArray.insertar("1");
-        listaArray.insertar("2");        
-        listaArray.insertar("3");
-        listaArray.insertar("4");
-        listaArray.insertar("5");        
-        listaArray.insertar("6");
-        listaArray.insertar("7");
-        listaArray.insertar("8");        
-        listaArray.insertar("9");
-        listaArray.insertar("10");
-        listaArray.insertar("11");        
-        listaArray.insertar("12");
-        listaArray.insertar("13");
-        listaArray.insertar("14");        
-        listaArray.insertar("15");
-        listaArray.insertar("16");
-        listaArray.insertar("17");        
-        listaArray.insertar("18");
-        
-        for(String valor:listaArray){
-            System.out.println(valor+" Con Iterable");
-            
-        }
-            
-            
-       
-        
-      }catch(Exception ex){
-          System.out.println(ex.getMessage());
-      }
-        
     }
-}    
-
+}
